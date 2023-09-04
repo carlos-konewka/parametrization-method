5d76df9655ab01389c082cb262095457daf95b78
carlos-konewka <pawel.gozdur@student.uj.edu.pl>
2023-09-04 02:43:12 +0200

    multiindices less or equal

--- multiindex.py
+++ multiindex.py
@@ -24,3 +24,13 @@ class Multiindex:
         first_part_cmp = np.array_equiv(d1, d2[:short_len])
         second_part_cmp = not d2[short_len:].any()
         return first_part_cmp and second_part_cmp
+
+    def __le__(self, other: Multiindex) -> bool:
+        for i, j in zip(self._data, other._data):
+            if i > j:
+                return False
+        if len(self._data) <= len(other._data):
+            return True
+        if not self._data[len(other._data):].any():
+            return True
+        return False
--- tests/test_multiindex.py
+++ tests/test_multiindex.py
@@ -28,7 +28,7 @@ class TestMultiindex(unittest.TestCase):
         self.assertEquals(data.dtype, 'int64')
         self.assertListEqual(list(data), list(data_sequence))
 
-    def test_equals_when_indices_are_equal(self):
+    def test_equality(self):
         # given
         u_data_sequence = np.asarray([1, 2, 3, 4])
         v_data_sequence = np.asarray([1, 2, 3, 4])
@@ -39,7 +39,7 @@ class TestMultiindex(unittest.TestCase):
         # then
         self.assertTrue(equal)
 
-    def test_equals_when_indices_are_not_equal(self):
+    def test_inequality(self):
         # given
         u_data_sequence = np.asarray([1, 2, 2, 4])
         v_data_sequence = np.asarray([1, 2, 3, 4])
@@ -50,7 +50,7 @@ class TestMultiindex(unittest.TestCase):
         # then
         self.assertFalse(equal)
 
-    def test_equals_when_indices_are_equal_and_lengths_are_different(self):
+    def test_equality_when_different_lengths(self):
         # given
         u_data_sequence = np.asarray([1, 2, 3, 4, 0])
         v_data_sequence = np.asarray([1, 2, 3, 4])
@@ -61,7 +61,7 @@ class TestMultiindex(unittest.TestCase):
         # then
         self.assertTrue(equal)
 
-    def test_equals_when_indices_are_not_equal_and_lengths_are_different(self):
+    def test_inequality_when_different_lengths(self):
         # given
         u_data_sequence = np.asarray([1, 2, 3, 4, 8])
         v_data_sequence = np.asarray([1, 2, 3, 4])
@@ -71,6 +71,51 @@ class TestMultiindex(unittest.TestCase):
         equal = u == v
         # then
         self.assertFalse(equal)
+
+    def test_leq(self):
+        # given
+        u = Multiindex([1, 3, 4, 2, 1])
+        v = Multiindex([2, 3, 4, 5, 6])
+        # when
+        leq = u <= v
+        # then
+        self.assertTrue(leq)
+
+    def test_non_leq_when_all_greater(self):
+        # given
+        u = Multiindex([2, 4, 5, 5, 6])
+        v = Multiindex([1, 3, 4, 2, 1])
+        # when
+        leq = u <= v
+        # then
+        self.assertFalse(leq)
+
+    def test_non_leq_when_at_least_one_greater(self):
+        # given
+        u = Multiindex([1, 3, 4, 2, 1])
+        v = Multiindex([1, 4, 5, 1, 1])
+        # when
+        leq = u <= v
+        # then
+        self.assertFalse(leq)
+
+    def test_leq_different_lengths(self):
+        # given
+        u = Multiindex([1, 2, 3, 0, 0])
+        v = Multiindex([3, 3, 3])
+        # when
+        leq = u <= v
+        # then
+        self.assertTrue(leq)
+
+    def test_non_leq_different_lengths(self):
+        # given
+        u = Multiindex([1, 2, 3, 0, 1])
+        v = Multiindex([3, 3, 3])
+        # when
+        leq = u <= v
+        # then
+        self.assertFalse(leq)
 
 
 if __name__ == '__main__':
